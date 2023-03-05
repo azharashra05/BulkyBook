1fbf390afa069f517c61e3292b2e8e072d79a85d
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-03-05 16:18:15 +0530

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
