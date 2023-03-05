148791ecc50bc7ba237590b930eabe979b494ea1
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-03-05 19:42:19 +0530

    Create Category View

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -13,8 +14,14 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            var _objCategoryList=_db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
         }
+
     }
 }
