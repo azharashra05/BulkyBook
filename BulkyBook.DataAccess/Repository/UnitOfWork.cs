788f5aec1ba7008848c5e018c916189ad02064b1
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-03-18 15:41:52 +0530

    Add CoverType model and create and implement CoverTypeRepository

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -15,9 +15,10 @@ namespace BulkyBook.DataAccess.Repository
         {
             _context = context;
             Category = new CategoryRepository(_context);
-
+            CoverType = new CoverTypeRepository(_context);
         }
         public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
 
         public void Save()
         {
